912668e1fa8b6c59dee81724b8c390d02a4bcab8
Mark Dobbins <mjdobbins12@gmail.com>
2019-10-28 19:56:14 +0000

    can check if a square is occupied

--- experimentation/test_move.py
+++ experimentation/test_move.py
@@ -1,18 +1,24 @@
 import move
+import pytest
 
 board = move.Board()
 
 class TestMove:
-        def test_move(self):
-                board.move(2,4,4,3)
-                assert board.show_board() == [
-								['B','B','B','B','B','B','B','B'],
-                ['B','B','B','B','B','B','B','B'],
-                ['B','B','B','B','B','B','B','B'],
-                ['B','B','B','B','B','B','B','B'],
-                ['B','B','B','*','B','B','B','B'],
-                ['B','B','B','B','B','B','B','B'],
-                ['B','B','B','B','B','B','B','B'],
-                ['B','B','B','B','B','B','B','B']
-              ]
-        
+    def test_move(self):
+        board.move(0,0,2,2)
+        assert board.show_board() == [
+    		['B','B','B','B','B','B','B','B'],
+            ['B','B','B','B','B','B','B','B'],
+            ['B','B','*','B','B','B','B','B'],
+            ['B','B','B','B','B','B','B','B'],
+            ['B','B','B','B','B','B','B','B'],
+            ['B','B','B','B','B','B','B','B'],
+            ['B','B','B','B','B','B','B','B'],
+    		['B','B','B','B','B','B','B','B']
+            ]
+
+    def test_blocking(self):
+        with pytest.raises(Exception) as excinfo:
+            board.grid[1][1] = '+'
+            board.move(0,0,1,1)
+        assert "Invalid move" in str(excinfo.value)
