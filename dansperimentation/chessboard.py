91ea5d74b29d2964ab3b11b36cdff72333cf45be
Dan Gyi <daniel.gyi@gmail.com>
2019-10-29 10:40:42 +0000

    tests passing for pawns moving 1/2 spaces

--- dansperimentation/chessboard.py
+++ dansperimentation/chessboard.py
@@ -1,14 +1,15 @@
 class ChessBoard:
-    board = [
-        ["R","N","B","K","Q","B","N","R"],
-        ['p','p','p','p','p','p','p','p'],
-        ["-","-","-","-","-","-","-","-"],
-        ["-","-","-","-","-","-","-","-"],
-        ["-","-","-","-","-","-","-","-"],
-        ["-","-","-","-","-","-","-","-"],
-        ['p','p','p','p','p','p','p','p'],
-        ["R","N","B","Q","K","B","N","R"]
-        ]
+    def __init__(self):
+        self.board = [
+            ["R","N","B","K","Q","B","N","R"],
+            ['p','p','p','p','p','p','p','p'],
+            ["-","-","-","-","-","-","-","-"],
+            ["-","-","-","-","-","-","-","-"],
+            ["-","-","-","-","-","-","-","-"],
+            ["-","-","-","-","-","-","-","-"],
+            ['p','p','p','p','p','p','p','p'],
+            ["R","N","B","Q","K","B","N","R"]
+            ]
 
     def show_board(self):
         return self.board
@@ -17,5 +18,6 @@ class ChessBoard:
         piece_to_move = self.board[start_row][start_col]
         self.board[start_row][start_col] = "-"
         self.board[end_row][end_col] = piece_to_move
+
         if piece_to_move == 'p' and abs(start_row - end_row) > 2:
             raise ValueError("Invalid Move")
--- dansperimentation/test_chessboard.py
+++ dansperimentation/test_chessboard.py
@@ -1,10 +1,15 @@
 import chessboard
 import pytest
 
-test_board = chessboard.ChessBoard()
+@pytest.fixture(autouse=True)
+def run_before_tests():
+        test_board = chessboard.ChessBoard()
+        return test_board
+
 
 class TestChessBoard:
-        def test_board_displayed(self):
+        def test_board_displayed(self, run_before_tests):
+                test_board = run_before_tests
                 assert test_board.show_board() == [
                 ["R","N","B","K","Q","B","N","R"],
                 ['p','p','p','p','p','p','p','p'],
@@ -16,18 +21,44 @@ class TestChessBoard:
                 ["R","N","B","Q","K","B","N","R"]
                 ]
 
-        def test_referencing_board(self):
-                assert test_board.show_board()[0][1] == ("N")
-                assert test_board.show_board()[5][3] == ("-")
-                assert test_board.show_board()[7][7] == ("R")
-                assert test_board.show_board()[0][3] != ("p")
+        def test_referencing_board(self, run_before_tests):
+                test_board = run_before_tests
+                assert test_board.board[0][1] == ("N")
+                assert test_board.board[5][3] == ("-")
+                assert test_board.board[7][7] == ("R")
+                assert test_board.board[0][3] != ("p")
 
-        def pawn_can_move_2_spaces_forward(self):
+        def test_white_pawn_can_move_1_space_forward(self, run_before_tests):
+                test_board = run_before_tests
+                test_board.move(6,1,5,1)
+                assert test_board.show_board()[5][1] == ("p")
+                assert test_board.show_board()[6][1] == ("-")
+
+        def test_black_pawn_can_move_1_space_forward(self, run_before_tests):
+                test_board = run_before_tests
+                test_board.move(1,2,2,2)
+                assert test_board.show_board()[2][2] == ('p')
+                assert test_board.show_board()[1][2] == ('-')
+
+
+        def test_white_pawn_can_move_2_spaces_forward(self, run_before_tests):
+                test_board = run_before_tests
                 test_board.move(6,1,4,1)
                 assert test_board.show_board()[4][1] == ("p")
                 assert test_board.show_board()[6][1] == ("-")
 
-        def test_pawn_cannot_move_3_spaces(self):
-                with pytest.raises(ValueError, match=r"Invalid Move"):
-                        test_board.move(6,1,3,1)
+        def test_black_pawn_can_move_2_spaces_forward(self, run_before_tests):
+                test_board = run_before_tests
+                test_board.move(1,2,3,2)
+                assert test_board.show_board()[3][2] == ('p')
+                assert test_board.show_board()[1][2] == ('-')
+                
+        
+        
+
+
+
+        # def test_pawn_cannot_move_3_spaces(self):
+        #         with pytest.raises(ValueError, match=r"Invalid Move"):
+        #                 test_board.move(6,1,3,1)
  
