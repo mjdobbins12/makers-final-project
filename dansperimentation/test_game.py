b4d38661a09e04ecc9a342c2b3d4141f42d61146
Volker Bergner <volker@bergner.one>
2019-10-29 14:29:16 +0000

    Improved mock of user input for testing

--- dansperimentation/test_game.py
+++ dansperimentation/test_game.py
@@ -21,7 +21,8 @@ class TestGame:
 
         def test_get_names(self):
             input = ['Batman', 'Superman']
-            test_game.input = lambda x: input.pop(0)
+            game.input = lambda x: input.pop(0)
+            test_game = game.Game()
             test_game.get_names()
             assert test_game.p1_name == 'Batman'
             assert test_game.p2_name == 'Superman'
