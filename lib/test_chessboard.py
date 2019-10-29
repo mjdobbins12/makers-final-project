6e41e21b114dcc2bd7068088373055e89953ef84
Dan Gyi <daniel.gyi@gmail.com>
2019-10-29 09:17:09 +0000

    refactor some experimental board designs/tests

--- lib/test_chessboard.py
+++ lib/test_chessboard.py
@@ -5,23 +5,19 @@ test_board = chessboard.ChessBoard()
 class TestChessBoard:
         def test_board_displayed(self):
                 assert test_board.show_board() == [
+                ["R","N","B","K","Q","B","N","R"],
+                ['p','p','p','p','p','p','p','p'],
                 ["-","-","-","-","-","-","-","-"],
                 ["-","-","-","-","-","-","-","-"],
                 ["-","-","-","-","-","-","-","-"],
                 ["-","-","-","-","-","-","-","-"],
-                ["-","-","-","-","-","-","-","-"],
-                ["-","-","-","-","-","-","-","-"],
-                ["-","-","-","-","-","-","-","-"],
-                ["-","-","-","-","-","-","-","-"]
+                ['p','p','p','p','p','p','p','p'],
+                ["R","N","B","Q","K","B","N","R"]
                 ]
 
         def test_referencing_board(self):
-                assert test_board.show_board()[0][1] == ("-")
+                assert test_board.show_board()[0][1] == ("N")
                 assert test_board.show_board()[5][3] == ("-")
-                assert test_board.show_board()[7][7] == ("-")
-
-        def test_populate_pawn_rows(self):
-                test_board.populate_pawn_rows()
-                assert test_board.show_board()[1][0] == "p"
-                assert test_board.show_board()[6][5] == "p"
-                assert test_board.show_board()[0][3] != "p"
+                assert test_board.show_board()[7][7] == ("R")
+                assert test_board.show_board()[0][3] != ("p")
+ 
