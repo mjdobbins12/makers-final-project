ff3e1d7971bb955cbdbef2fda8c7a16351486692
Mike <mjwalshe@gmail.com>
2019-10-30 19:47:26 +0000

    Fix L-shaped move rule on knight

--- MVP/knight.py
+++ MVP/knight.py
@@ -11,9 +11,9 @@ class Knight(Piece):
         
         def illegal_directions(self, board, start_row, start_col, end_row, end_col):
                 return any([
-                        (not abs(start_row - end_row) == 1 and abs(start_col - end_col) == 2) or # vertical L shape
-                        (not abs(start_row - end_row) == 2 and abs(start_col - end_col) == 1),
-                        (self.__knight_specific_board_constraints(board, start_row, start_col, end_row, end_col))  # horizontal L shape
+                        not (abs(start_row - end_row) == 1 and abs(start_col - end_col) == 2 or # vertical L shape
+                        abs(start_row - end_row) == 2 and abs(start_col - end_col) == 1), # horizontal L shape
+                        (self.__knight_specific_board_constraints(board, start_row, start_col, end_row, end_col))  
                         ])
 
         def __knight_specific_board_constraints(self, board, start_row, start_col, end_row, end_col):
