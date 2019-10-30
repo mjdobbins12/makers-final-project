0c47feaa1180390e3e6c412447f2a45f9c3b931e
Dan Gyi <daniel.gyi@gmail.com>
2019-10-30 15:54:50 +0000

    refactor constraints to piece classes

--- MVP/knight.py
+++ MVP/knight.py
@@ -9,9 +9,19 @@ class Knight(Piece):
                 elif self.colour == "White":
                         self.symbol = "♘"
         
-        def illegal_directions(self, start_row, start_col, end_row, end_col):
-               return any([
+        def illegal_directions(self, board, start_row, start_col, end_row, end_col):
+                return any([
                         (not abs(start_row - end_row) == 1 and abs(start_col - end_col) == 2) or # vertical L shape
-                        not abs(start_row - end_row) == 2 and abs(start_col - end_col) == 1 # horizontal L shape 
+                        (not abs(start_row - end_row) == 2 and abs(start_col - end_col) == 1),
+                        (self.__knight_specific_board_constraints(board, start_row, start_col, end_row, end_col))  # horizontal L shape
                         ])
+
+        def __knight_specific_board_constraints(self, board, start_row, start_col, end_row, end_col):
+                piece_to_move = board[start_row][start_col]
+                return any([
+                        hasattr(board[end_row][end_col], 'colour') and
+                        # knight cannot take any piece of same colour
+                        piece_to_move.colour == board[end_row][end_col].colour
+                        ]
+                        )
                         
