bc0f5459409d0c8a6f2f1829a237fd4ee8df4e7e
Dan Gyi <daniel.gyi@gmail.com>
2019-10-30 16:24:25 +0000

    refactor bishop

--- MVP/bishop.py
+++ MVP/bishop.py
@@ -9,7 +9,45 @@ class Bishop(Piece):
         elif self.colour == "White":
             self.symbol = "♗"
 
-    def illegal_directions(self, start_row, start_col, end_row, end_col):
-        return (
-            abs(start_row - end_row) != abs(start_col - end_col) # must move the same absolute number of squares in x and y directions
-            )
+    def illegal_directions(self, board, start_row, start_col, end_row, end_col):
+        return any([
+            abs(start_row - end_row) != abs(start_col - end_col), # must move the same absolute number of squares in x and y directions
+            self.__bishop_specific_board_constraints(board, start_row, start_col, end_row, end_col)
+            ])
+
+    def __bishop_specific_board_constraints(self, board, start_row, start_col, end_row, end_col):
+                return (self.__check_if_diagonal_blocked(board, start_row, start_col, end_row, end_col))
+
+    def __check_if_diagonal_blocked(self, board, start_row, start_col, end_row, end_col):
+            if start_row < end_row and start_col < end_col:
+                    check_square = board[start_row + 1][start_col + 1]
+                    if isinstance(check_square, Piece):
+                            return True
+                    elif check_square == board[end_row][end_col]:
+                            return False
+                    else:
+                            self.__check_if_diagonal_blocked(self, start_row + 1, start_col + 1, end_row, end_col)
+            elif start_row > end_row and start_col > end_col:
+                    check_square = board[start_row - 1][start_col - 1]
+                    if isinstance(check_square, Piece):
+                            return True
+                    elif check_square == board[end_row][end_col]:
+                            return False
+                    else:
+                            self.__check_if_diagonal_blocked(self, start_row - 1, start_col - 1, end_row, end_col)
+            elif start_row < end_row and start_col > end_col:
+                    check_square = board[start_row + 1][start_col - 1]
+                    if isinstance(check_square, Piece):
+                            return True
+                    elif check_square == board[end_row][end_col]:
+                            return False
+                    else:
+                            self.__check_if_diagonal_blocked(self, start_row + 1, start_col - 1, end_row, end_col)
+            elif start_row > end_row and start_col < end_col:
+                    check_square = board[start_row - 1][start_col + 1]
+                    if isinstance(check_square, Piece):
+                            return True
+                    elif check_square == board[end_row][end_col]:
+                            return False
+                    else:
+                            self.__check_if_diagonal_blocked(self, start_row - 1, start_col + 1, end_row, end_col)
