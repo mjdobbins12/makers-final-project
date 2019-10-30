2316eb08e6def2ef29d28a4cd60d6a21d4f0859f
Mike <mjwalshe@gmail.com>
2019-10-30 14:01:33 +0000

    Add symbols to knight class

--- MVP/knight.py
+++ MVP/knight.py
@@ -1,10 +1,11 @@
 class Knight:
         def __init__(self, colour):
                 self.colour = colour
+                self.name = "Knight"
                 if self.colour == "Black":
-                        self.name = 'Knight'
+                        self.symbol = '♞'
                 elif self.colour == "White":
-                        self.name = "Knight"
+                        self.symbol = "♘"
         
         def illegal_directions(self, start_row, start_col, end_row, end_col):
                return any([
