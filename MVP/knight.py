48c1906854a15af90398d8720752099dd83f947d
Mike <mjwalshe@gmail.com>
2019-10-30 11:05:14 +0000

    Create Knight class

---
+++ MVP/knight.py
@@ -0,0 +1,10 @@
+class Knight:
+        def __init__(self, colour):
+                self.colour = colour
+                if self.colour == "Black":
+                        self.name = 'N'
+                elif self.colour == "White":
+                        self.name = "N"
+        
+        def illegal_directions(self, start_row, start_col, end_row, end_col):
+                return 
