cc46f3ddd49939662e45f54be3be1c7bc140a32c
Dan Gyi <daniel.gyi@gmail.com>
2019-10-30 16:59:36 +0000

    refactor move logic - queen

--- MVP/queen.py
+++ MVP/queen.py
@@ -10,6 +10,10 @@ class Queen(Piece):
                         self.symbol = '♕'
 
         def invalid_move_types(self, start_row, start_col, end_row, end_col):
+                return any([self.__can_only_move_like_rook_or_bishop(start_row, start_col, end_row, end_col),
+                ])
+
+        def __can_only_move_like_rook_or_bishop(self, start_row, start_col, end_row, end_col):
                 if abs(start_row - end_row) > 1 and abs(start_col - end_col) != 0 and abs(start_col - end_col) != abs(start_row - end_row):
                         return True
                 elif abs(start_col - end_col) > 1 and abs(start_row - end_row) != 0 and abs(start_row - end_row) != abs(start_col - end_col):
