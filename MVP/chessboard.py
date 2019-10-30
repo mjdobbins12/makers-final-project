69fb0e2c117820807bb789c3032753fb68dcbb3b
Mike <mjwalshe@gmail.com>
2019-10-30 13:45:49 +0000

    Refactor board to check piece type when running constraints

--- MVP/chessboard.py
+++ MVP/chessboard.py
@@ -27,37 +27,38 @@ class ChessBoard:
 
         def __invalid_move(self, start_row, start_col, end_row, end_col):
                 piece_to_move = self.board[start_row][start_col]
-                if piece_to_move.name == "Pawn":
-                        return any(
-                                [self.__check_within_board_boundary(end_row,end_col),
-                                piece_to_move.illegal_directions(start_row, start_col, end_row, end_col), # checks pawn allowed vectors
-                                self.__pawn_specific_board_constraints(start_row, start_col, end_row, end_col) # references board to check possibility of moves
-                                ]
-                                )
-                elif piece_to_move.name == "Knight":
-                         return any(
-                                [self.__check_within_board_boundary(end_row,end_col),
-                                piece_to_move.illegal_directions(start_row, start_col, end_row, end_col), # checks knight allowed vectors
-                                self.__knight_specific_board_constraints(start_row, start_col, end_row, end_col) # references board to check possibility of moves
-                                ]
-                                )
+                return any(
+                        [self.__check_within_board_boundary(end_row,end_col),
+                        piece_to_move.illegal_directions(start_row, start_col, end_row, end_col), # checks pawn allowed vectors
+                        self.__knight_specific_board_constraints(start_row, start_col, end_row, end_col), # references board to check possibility of moves
+                        self.__pawn_specific_board_constraints(start_row, start_col, end_row, end_col) # references board to check possibility of moves
+                        ]
+                        )
+
 
         def __check_within_board_boundary(self, end_row, end_col):
                 return (end_row > 7 or end_col > 7 or end_row < 0 or end_col < 0)
 
         def __pawn_specific_board_constraints(self, start_row, start_col, end_row, end_col):
                 piece_to_move = self.board[start_row][start_col]
-                return any([start_col == end_col and isinstance(self.board[end_row][end_col], pawn.Pawn), # cannot move forward one space into another pawn
-                        (abs(start_col - end_col) == 1 and abs(start_row - end_row) == 1 and not isinstance(self.board[end_row][end_col], pawn.Pawn)), # can only strike if pawn on target square
-                        piece_to_move.colour == "White" and abs(end_row - start_row) == 2 and isinstance(self.board[end_row+1][end_col], pawn.Pawn), # black cannot jump over pawn
-                        piece_to_move.colour == "Black" and abs(end_row - start_row) == 2 and isinstance(self.board[end_row-1][end_col], pawn.Pawn) # white cannot jump over pawn
-                        ]
-                        )
+                if isinstance(piece_to_move, pawn.Pawn):
+                        return any([start_col == end_col and isinstance(self.board[end_row][end_col], pawn.Pawn), # cannot move forward one space into another pawn
+                                (abs(start_col - end_col) == 1 and abs(start_row - end_row) == 1 and not isinstance(self.board[end_row][end_col], pawn.Pawn)), # can only strike if pawn on target square
+                                piece_to_move.colour == "White" and abs(end_row - start_row) == 2 and isinstance(self.board[end_row+1][end_col], pawn.Pawn), # black cannot jump over pawn
+                                piece_to_move.colour == "Black" and abs(end_row - start_row) == 2 and isinstance(self.board[end_row-1][end_col], pawn.Pawn) # white cannot jump over pawn
+                                ]
+                                )
                 
         def __knight_specific_board_constraints(self, start_row, start_col, end_row, end_col):
                 piece_to_move = self.board[start_row][start_col]
-                return any([
-                        hasattr(self.board[end_row][end_col], 'colour') and 
-                                piece_to_move.colour == self.board[end_row][end_col].colour # knight cannot take any piece of same colour
-                        ]
-                        )
+                if isinstance(piece_to_move, knight.Knight):
+                        return any([
+                                hasattr(self.board[end_row][end_col], 'colour') and 
+                                        piece_to_move.colour == self.board[end_row][end_col].colour # knight cannot take any piece of same colour
+                                ]
+                                )
+
+                
+                
+                
+                
