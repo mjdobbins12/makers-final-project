431985b56577ebdccf1f7416f20e5a0f91cf7735
Mike <mjwalshe@gmail.com>
2019-10-30 18:03:14 +0000

    Update standard rule set

--- MVP/standard_rules.py
+++ MVP/standard_rules.py
@@ -4,12 +4,18 @@ import rook
 import piece
 import knight
 
-# Allows rules to be extracted with tests passimg. Starting board 
+# Allows rules to be extracted with tests passing. Starting board 
 # is taken from rule set, then gameplay happens in that instance of board inside
 # the ChessBoard instance. 
 # Moves are checked against the current board by the ruleset, against its 
 # stored rules. A bit of back and forth between ChessBoard and RuleSet, but clearly
 # preferable to the game taking place within the ruleset object. 
+
+# Board boundary is defined inside this object. 
+# Piece specific rules remain on the pieces themselves. This does lead to 
+# Some flexibility; by creating a different starting board below with special variants of pieces
+# we could introduce different constraints to the game. However, would need more methods to 
+# be able to change rules mid-game
 
 class StandardRules:
         
@@ -30,7 +36,6 @@ class StandardRules:
         def invalid_move(self, board, start_row, start_col, end_row, end_col):
                 current_board = board
                 piece_to_move = current_board[start_row][start_col]
-                # print(piece_to_move)
                 return any(
                         [self.check_within_board_boundary(end_row,end_col),
                         (piece_to_move.illegal_directions(current_board, start_row, start_col, end_row, end_col)) # checks pawn allowed vectors
@@ -39,3 +44,4 @@ class StandardRules:
 
         def check_within_board_boundary(self, end_row, end_col):
                 return (end_row > 7 or end_col > 7 or end_row < 0 or end_col < 0)
+        
