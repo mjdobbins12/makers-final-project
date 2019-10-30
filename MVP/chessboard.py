ed919ec6eb03f4c92223973d6d833752252d3bfe
Mark Dobbins <mjdobbins12@gmail.com>
2019-10-30 17:34:56 +0000

    kings appear on board

--- MVP/chessboard.py
+++ MVP/chessboard.py
@@ -3,11 +3,12 @@ import bishop
 import rook
 import piece
 import knight
+import king
 
 class ChessBoard:
         def __init__(self):
                 self.board = [
-                        [rook.Rook("Black"),knight.Knight("Black"),bishop.Bishop("Black"),"Q","K",bishop.Bishop("Black"),knight.Knight("Black"),rook.Rook("Black")],
+                        [rook.Rook("Black"),knight.Knight("Black"),bishop.Bishop("Black"),"Q",king.King("Black"),bishop.Bishop("Black"),knight.Knight("Black"),rook.Rook("Black")],
                         [pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black"),
                         pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black")],
                         ["-","-","-","-","-","-","-","-"],
@@ -16,7 +17,7 @@ class ChessBoard:
                         ["-","-","-","-","-","-","-","-"],
                         [pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White"),
                         pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White")],
-                        [rook.Rook("White"),knight.Knight("White"),bishop.Bishop("White"),"Q","K",bishop.Bishop("White"),knight.Knight("White"),rook.Rook("White")]
+                        [rook.Rook("White"),knight.Knight("White"),bishop.Bishop("White"),"Q",king.King("White"),bishop.Bishop("White"),knight.Knight("White"),rook.Rook("White")]
                         ]
 
         def move(self, start_row, start_col, end_row, end_col):
@@ -40,5 +41,3 @@ class ChessBoard:
 
         def __check_within_board_boundary(self, end_row, end_col):
                 return (end_row > 7 or end_col > 7 or end_row < 0 or end_col < 0)
-
-        
