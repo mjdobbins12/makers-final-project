382a7f32ced67d2ee5cb36f35bece4244442f4cb
Dan Gyi <daniel.gyi@gmail.com>
2019-10-30 10:40:37 +0000

    rook can move laterally

--- MVP/test_rook.py
+++ MVP/test_rook.py
@@ -18,3 +18,6 @@ class TestIllegalMoves:
 
 	def test_rook_cannot_move_diagonally(self):
 		assert test_rook_b.illegal_directions(5, 5, 4, 4) == True
+
+	def test_rook_can_move_laterally(self):
+		assert test_rook_b.illegal_directions(5, 5, 5, 6) == False
