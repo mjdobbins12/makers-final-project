b2514d29ed987aa24565e6540ec97bc8db6b419c
Dan Gyi <daniel.gyi@gmail.com>
2019-10-31 16:39:24 +0000

    refactor with tests passing

--- MVP/game.py
+++ MVP/game.py
@@ -1,5 +1,7 @@
 import chessboard
 import turn
+from king import King
+from piece import Piece
 
 class Game:
 
@@ -29,3 +31,32 @@ class Game:
         colour = 'White' if self.p1_turn else 'Black'
         if self.board.board[x][y].colour != colour:
             raise ValueError("PlayerDoesNotOwnPiece")
+
+
+    def in_check(self, board):
+        check_evaluations = []
+        king_coordinates = self.__find_current_colour_king(board)
+        for i in range(0,8):
+            for j in range(0,8):
+                if isinstance(board[i][j], Piece):
+                    if [king_coordinates[0],king_coordinates[1]] in board[i][j].available_moves(board, i, j):
+                        check_evaluations.append(True)
+                    else:
+                        check_evaluations.append(False)
+        if True in check_evaluations:
+            return True
+        else:
+            return False
+    
+    def __find_current_colour_king(self, board):
+        for i in range(0,8):
+            for j in range(0,8):
+                if self.p1_turn:
+                    if isinstance(board[i][j], King):
+                        if board[i][j].colour == "White":
+                            return [i,j]
+                else:
+                    if isinstance(board[i][j], King):
+                        if board[i][j].colour == "Black":
+                            return [i,j]
+
