bb413d10608edf2f2fd6d788fa85471ea1ca9a2b
Dan Gyi <daniel.gyi@gmail.com>
2019-10-31 11:24:34 +0000

    started rework of diagonal_block_check

--- MVP/piece.py
+++ MVP/piece.py
@@ -12,7 +12,28 @@ class Piece:
         # shared movement constraints, e.g. for bishop + queen, rook + queen
 
         def check_if_diagonal_blocked(self, board, start_row, start_col, end_row, end_col):
-                piece_to_move = board[start_row][start_col]
+                # piece_to_move = board[start_row][start_col]
+                # if start_row > end_row:
+                #                 rows_between = list(
+                #                 range(end_row + 1, start_row))
+                #         else:
+                #                 rows_between = list(
+                #                 range(start_row + 1, end_row))
+
+
+                # if start_col > end_col:
+                #                 cols_between = list(
+                #                 range(end_col + 1, start_col))
+                #         else:
+                #                 cols_between = list(
+                #                 range(start_col + 1, end_col))
+
+                #         squares_between[:] = [board[element][start_col]
+                #                         for element in squares_between]
+                #         if any(isinstance(x, Piece) for x in squares_between):
+                #                 return True
+
+
                 if start_row < end_row and start_col < end_col:
                     check_square = board[start_row + 1][start_col + 1]
                     if isinstance(check_square, Piece) and check_square.colour == piece_to_move.colour:
@@ -20,7 +41,7 @@ class Piece:
                     elif check_square == board[end_row][end_col]:
                         return False
                     else:
-                        self.check_if_diagonal_blocked(self, start_row + 1, start_col + 1, end_row, end_col)
+                        self.check_if_diagonal_blocked(self, board, start_row + 1, start_col + 1, end_row, end_col)
                 elif start_row > end_row and start_col > end_col:
                     check_square = board[start_row - 1][start_col - 1]
                     if isinstance(check_square, Piece) and check_square.colour == piece_to_move.colour:
@@ -28,7 +49,7 @@ class Piece:
                     elif check_square == board[end_row][end_col]:
                         return False
                     else:
-                        self.check_if_diagonal_blocked(self, start_row - 1, start_col - 1, end_row, end_col)
+                        self.check_if_diagonal_blocked(self, board, start_row - 1, start_col - 1, end_row, end_col)
                 elif start_row < end_row and start_col > end_col:
                     check_square = board[start_row + 1][start_col - 1]
                     if isinstance(check_square, Piece) and check_square.colour == piece_to_move.colour:
@@ -36,7 +57,7 @@ class Piece:
                     elif check_square == board[end_row][end_col]:
                         return False
                     else:
-                        self.check_if_diagonal_blocked(self, start_row + 1, start_col - 1, end_row, end_col)
+                        self.check_if_diagonal_blocked(self, board, start_row + 1, start_col - 1, end_row, end_col)
                 elif start_row > end_row and start_col < end_col:
                     check_square = board[start_row - 1][start_col + 1]
                     if isinstance(check_square, Piece) and check_square.colour == piece_to_move.colour:
@@ -44,7 +65,7 @@ class Piece:
                     elif check_square == board[end_row][end_col]:
                         return False
                     else:
-                        self.check_if_diagonal_blocked(self, start_row - 1, start_col + 1, end_row, end_col)
+                        self.check_if_diagonal_blocked(self, board, start_row - 1, start_col + 1, end_row, end_col)
 
         def check_if_row_or_column_blocked(self, board, start_row, start_col, end_row, end_col):
                 if start_row == end_row:
