7d08eab1db91c8bcf0ffb5d73411cdb8e6159454
Dan Gyi <daniel.gyi@gmail.com>
2019-10-31 14:32:37 +0000

    test scholars mate - pass

--- MVP/test_checkmate.py
+++ MVP/test_checkmate.py
@@ -14,10 +14,23 @@ class TestCheckmate:
                 test_board = run_before_tests
                 assert test_board.board[0][4].is_checkmate(test_board.board, 0, 4) == False
 
-        def test_is_checkmate(self, run_before_tests):
+        # fool's mate
+        def test_fools_mate_is_checkmate(self, run_before_tests):
                 test_board = run_before_tests
                 test_board.move(6,5,5,5)
                 test_board.move(1,4,3,4)
                 test_board.move(6,6,4,6)
                 test_board.move(0,3,4,7)
                 assert test_board.board[7][4].is_checkmate(test_board.board, 7, 4) == True
+
+        # scholar's mate
+        def test_scholars_mate_is_checkmate(self, run_before_tests):
+                test_board = run_before_tests
+                test_board.move(6,4,4,4)
+                test_board.move(1,4,3,4)
+                test_board.move(7,5,4,2)
+                test_board.move(1,0,2,0)
+                test_board.move(7,3,5,5)
+                test_board.move(2,0,3,0)
+                test_board.move(5,5,1,5)
+                assert test_board.board[0][4].is_checkmate(test_board.board, 0, 4) == True
