181cbcdcd0d8fb1f9928f728d65ba8fad044ecbf
Dan Gyi <daniel.gyi@gmail.com>
2019-11-01 10:38:46 +0000

    checkmate class updated with tests

--- MVP/checkmate.py
+++ MVP/checkmate.py
@@ -5,19 +5,20 @@ from piece import Piece
 from game import Game
 
 class Checkmate:
+        def __init__(self, game):
+                self.game = game
 
-        def is_checkmate(self, game):
-                king_coords = game.__find_current_colour_king(self.board.board)
-                king_in_question = self.board.board[king_coords[0]][king_coords[1]]
+        def is_checkmate(self):
+                king_coords = self.__find_current_colour_king()
+                king_in_question = self.game.board.board[king_coords[0]][king_coords[1]]
                 checkmate_evaluations = []
-                if king_in_question.in_check(self.board.board, king_coords[0], king_coords[1]) == True:
-                    possible_king_moves_array = self.available_moves(self.board.board, king_coords[0], king_coords[1])
+                if king_in_question.in_check(self.game.board.board, king_coords[0], king_coords[1]) == True:
+                    possible_king_moves_array = king_in_question.available_moves(self.game.board.board, king_coords[0], king_coords[1])
                     for coords in possible_king_moves_array:
-                        if self.in_check(self.board.board, coords[0], coords[1]):
+                        if king_in_question.in_check(self.game.board.board, coords[0], coords[1]):
                             checkmate_evaluations.append(True)
                         else:
                             checkmate_evaluations.append(False)
-                    print(checkmate_evaluations)
                     if False not in checkmate_evaluations:
                         return True
                     else:
@@ -25,14 +26,14 @@ class Checkmate:
                 else:
                     return False
 
-        def __find_current_colour_king(self, game):
+        def __find_current_colour_king(self):
                 for i in range(0,8):
                     for j in range(0,8):
-                        if self.p1_turn:
-                            if isinstance(board[i][j], King):
-                                if board[i][j].colour == "White":
+                        if self.game.p1_turn:
+                            if isinstance(self.game.board.board[i][j], King):
+                                if self.game.board.board[i][j].colour == "White":
                                     return [i,j]
                         else:
-                            if isinstance(board[i][j], King):
-                                if board[i][j].colour == "Black":
+                            if isinstance(self.game.board.board[i][j], King):
+                                if self.game.board.board[i][j].colour == "Black":
                                     return [i,j]
