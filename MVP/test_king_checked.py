ff8b11a5d487f75e6ec0183e91619cd0c8918eb6
Mark Dobbins <mjdobbins12@gmail.com>
2019-11-01 16:52:45 +0000

    test that both kings start out of check

--- MVP/test_king_checked.py
+++ MVP/test_king_checked.py
@@ -13,11 +13,12 @@ class TestKingInCheck:
         def test_starting_king_not_in_check(self, run_before_tests):
                 test_game = run_before_tests
                 assert test_game.board.board[0][4].in_check(test_game.board.board, 0, 4) == False
-        
+                assert test_game.board.board[7][4].in_check(test_game.board.board, 0, 4) == False
+
         def test_king_can_be_in_check(self, run_before_tests):
                 test_game = run_before_tests
                 test_game.execute_turn(6,3,4,3)
                 test_game.execute_turn(1,2,2,2)
                 test_game.execute_turn(6,7,4,7)
                 test_game.execute_turn(0,3,3,0)
-                assert test_game.board.board[7][4].in_check(test_game.board.board, 7, 4) == True 
+                assert test_game.board.board[7][4].in_check(test_game.board.board, 7, 4) == True
