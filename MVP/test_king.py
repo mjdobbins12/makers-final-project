b6e3e99cdfbd1153089983d6f4fd3c0aa3c7870a
Dan Gyi <daniel.gyi@gmail.com>
2019-11-01 12:40:30 +0000

    2 tests for adjacent kings failing

--- MVP/test_king.py
+++ MVP/test_king.py
@@ -3,11 +3,17 @@ from piece import Piece
 import chessboard
 import king
 import rook
+import game
 
 @pytest.fixture(autouse=True)
-def run_before_tests():
+def board_before_tests():
         test_board = chessboard.ChessBoard()
         return test_board
+
+@pytest.fixture(autouse=True)   
+def game_before_tests():
+        test_game = game.Game("p1", "p2")
+        return test_game
 
 test_king_b = king.King("Black")
 test_king_w = king.King("White")
@@ -26,38 +32,38 @@ class TestKingProperties:
         assert test_king_w.counter == 0
 
 class TestKingMovement:
-    def test_king_can_move_up(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_can_move_up(self, board_before_tests):
+        test_board = board_before_tests
         assert test_king_b.illegal_directions(test_board.board,4,4,3,4) == False
 
-    def test_king_can_move_down(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_can_move_down(self, board_before_tests):
+        test_board = board_before_tests
         assert test_king_b.illegal_directions(test_board.board,4,4,5,4) == False
 
-    def test_king_can_move_sideways(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_can_move_sideways(self, board_before_tests):
+        test_board = board_before_tests
         assert test_king_b.illegal_directions(test_board.board,4,4,4,5) == False
 
-    def test_king_can_move_diagonally(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_can_move_diagonally(self, board_before_tests):
+        test_board = board_before_tests
         assert test_king_b.illegal_directions(test_board.board,4,4,3,3) == False
 
-    def test_king_cannot_move_two_squares_laterally(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_cannot_move_two_squares_laterally(self, board_before_tests):
+        test_board = board_before_tests
         assert test_king_b.illegal_directions(test_board.board,4,4,4,2) == True
 
-    def test_king_cannot_move_two_squares_vertically(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_cannot_move_two_squares_vertically(self, board_before_tests):
+        test_board = board_before_tests
         assert test_king_b.illegal_directions(test_board.board,3,4,5,4) == True
 
-    def test_king_cannot_move_two_squares_diagonally(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_cannot_move_two_squares_diagonally(self, board_before_tests):
+        test_board = board_before_tests
         assert test_king_b.illegal_directions(test_board.board,4,4,2,2) == True
 
 class TestCastling:
 
-    def test_king_can_castle_if_it_and_rook_havent_moved(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_can_castle_if_it_and_rook_havent_moved(self, board_before_tests):
+        test_board = board_before_tests
         test_board.move(6,6,5,6)
         test_board.move(1,0,3,0)
         test_board.move(7,5,6,6)
@@ -70,8 +76,8 @@ class TestCastling:
         assert test_board.board[7][4] == '-'
         assert test_board.board[7][7] == '-'
 
-    def test_castling_forbidden_if_king_has_moved(self, run_before_tests):
-        test_board = run_before_tests
+    def test_castling_forbidden_if_king_has_moved(self, board_before_tests):
+        test_board = board_before_tests
         test_board.move(6,6,5,6)
         test_board.move(1,0,3,0)
         test_board.move(7,5,6,6)
@@ -85,8 +91,8 @@ class TestCastling:
         with pytest.raises(ValueError, match=r"Invalid Move"):
                     test_board.move(7,4,7,6)
 
-    def test_castling_forbidden_if_rook_has_moved(self, run_before_tests):
-        test_board = run_before_tests
+    def test_castling_forbidden_if_rook_has_moved(self, board_before_tests):
+        test_board = board_before_tests
         test_board.move(6,6,5,6)
         test_board.move(1,0,3,0)
         test_board.move(7,5,6,6)
@@ -100,11 +106,36 @@ class TestCastling:
         with pytest.raises(ValueError, match=r"Invalid Move"):
                     test_board.move(7,4,7,6)
 
-    def test_king_cant_castle_over_other_piece(self, run_before_tests):
-        test_board = run_before_tests
+    def test_king_cant_castle_over_other_piece(self, board_before_tests):
+        test_board = board_before_tests
         test_board.move(7,6,5,5)
         with pytest.raises(ValueError, match=r"Invalid Move"):
                     test_board.move(7,4,7,6)
+
+class TestKingsCantBeAdjacent:
+    def test_kings_cant_be_adjacent(self, game_before_tests):
+        test_game = game_before_tests
+        test_game.execute_turn(6,4,4,4)
+        test_game.execute_turn(1,4,3,4)
+        test_game.execute_turn(7,4,6,4)
+        test_game.execute_turn(0,4,1,4)
+        test_game.execute_turn(6,4,5,5)
+        test_game.execute_turn(1,4,2,5)
+        test_game.execute_turn(5,5,4,6)
+        with pytest.raises(ValueError, match=r"Invalid Move"):
+                    test_game.execute_turn(2,5,3,6)
+
+    def test_kings_cant_be_adjacent2(self, game_before_tests):
+        test_game = game_before_tests
+        test_game.execute_turn(6,4,4,4)
+        test_game.execute_turn(1,4,3,4)
+        test_game.execute_turn(7,4,6,4)
+        test_game.execute_turn(0,4,1,4)
+        test_game.execute_turn(6,4,5,3)
+        test_game.execute_turn(1,4,2,3)
+        test_game.execute_turn(5,3,4,2)
+        with pytest.raises(ValueError, match=r"Invalid Move"):
+                    test_game.execute_turn(2,3,3,2)
 
         
 
