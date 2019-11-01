0af55bcdf8269039dae6e3cd10aaab28c399bb14
Dan Gyi <daniel.gyi@gmail.com>
2019-11-01 11:17:07 +0000

    refactor test king checked

--- MVP/test_king_checked.py
+++ MVP/test_king_checked.py
@@ -1,25 +1,23 @@
 import pytest
 from piece import Piece
-from chessboard import ChessBoard
 from king import King
 from game import Game
 
 @pytest.fixture(autouse=True)
 def run_before_tests():
         test_game = Game("p1", "p2")
-        test_board = ChessBoard()
-        return test_board
+        return test_game
 
 class TestKingInCheck:
 
         def test_starting_king_not_in_check(self, run_before_tests):
-                test_board = run_before_tests
-                assert test_board.board[0][4].in_check(test_board.board, 0, 4) == False
+                test_game = run_before_tests
+                assert test_game.board.board[0][4].in_check(test_game.board.board, 0, 4) == False
         
         def test_king_can_be_in_check(self, run_before_tests):
-                test_board = run_before_tests
-                test_board.move(6,3,4,3)
-                test_board.move(1,2,2,2)
-                test_board.move(6,7,4,7)
-                test_board.move(0,3,3,0)
-                assert test_board.board[7][4].in_check(test_board.board, 7, 4) == True 
+                test_game = run_before_tests
+                test_game.execute_turn(6,3,4,3)
+                test_game.execute_turn(1,2,2,2)
+                test_game.execute_turn(6,7,4,7)
+                test_game.execute_turn(0,3,3,0)
+                assert test_game.board.board[7][4].in_check(test_game.board.board, 7, 4) == True 
