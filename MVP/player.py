4bf1953b98947a0e3789207378419c5e46983a7e
Mark Dobbins <mjdobbins12@gmail.com>
2019-11-01 11:15:24 +0000

    piece storage for player class

--- MVP/player.py
+++ MVP/player.py
@@ -2,3 +2,7 @@ class Player:
         def __init__(self, name, colour):
                 self.name = name
                 self.colour = colour
+                self.taken_pieces = []
+
+        def store_piece(self, piece):
+                self.taken_pieces.append(piece)
--- MVP/test_player.py
+++ MVP/test_player.py
@@ -1,8 +1,10 @@
 import pytest
 
+import pawn
 import player
 
 test_player_1 = player.Player("test_name_1", "White")
+test_pawn = pawn.Pawn("Black")
 
 class TestPlayerProperties:
         def test_player_name(self):
@@ -10,3 +12,11 @@ class TestPlayerProperties:
 
         def test_player_colour(self):
                 assert test_player_1.colour == "White"
+
+        def test_player_captured_pieces_storage(self):
+                assert test_player_1.taken_pieces == []
+
+class TestPlayerActions:
+        def test_player_captured_pieces(self):
+                test_player_1.store_piece(test_pawn)
+                assert test_player_1.taken_pieces == [test_pawn]
