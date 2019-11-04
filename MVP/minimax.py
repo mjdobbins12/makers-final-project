cb74594a11e007193e263eed79b11f9551a9caab
Dan Gyi <daniel.gyi@gmail.com>
2019-11-04 11:15:49 +0000

    testing bug resolved

--- MVP/minimax.py
+++ MVP/minimax.py
@@ -1,18 +1,12 @@
 from piece import Piece
 
 class Minimax:
-        def __init__(self, game, p1_turn):
-                self.board = game.board.board
+        def __init__(self, game):
                 self.game = game
-                if p1_turn == True:
-                        self.turn = "White"
-                else:
-                        self.turn = "Black"
         
         def execute_turn(self):
                 return []
 
-        
         def move_value(self):
                 return []
 
@@ -27,9 +21,15 @@ class Minimax:
                 array = []
                 for i in range(0,8):
                         for j in range(0,8):
-                                if isinstance(self.board[i][j], Piece):
-                                        if self.board[i][j].colour == self.turn:
-                                                array.append(self.board[i][j].available_moves(self.board, i, j))
+                                if isinstance(self.game.board.board[i][j], Piece):
+                                        if self.game.p1_turn == True:
+                                                if self.game.board.board[i][j].colour == "White":
+                                                        print([self.game.board.board[i][j].colour, self.game.board.board[i][j].name, i,j])
+                                                        array.append(self.game.board.board[i][j].available_moves(self.game.board.board, i, j))
+                                        elif self.game.p1_turn == False:
+                                                if self.game.board.board[i][j].colour == "Black":
+                                                        print([self.game.board.board[i][j].colour, self.game.board.board[i][j].name, i,j])
+                                                        array.append(self.game.board.board[i][j].available_moves(self.game.board.board, i, j))
                 array = list(filter(lambda a: a != [], array)) # removes empty arrays from available moves array
                 return array
 
--- MVP/test_minimax.py
+++ MVP/test_minimax.py
@@ -6,17 +6,17 @@ game = game.Game('p1', 'p2')
 
 @pytest.fixture(autouse=True)
 def run_before_tests():        
-        test_minimax = minimax.Minimax(game, game.p1_turn)
+        test_minimax = minimax.Minimax(game)
         return test_minimax
 
 class TestInputOutput:
         def test_accepts_two_args(self, run_before_tests):
                 test_minimax = run_before_tests
-                new_minimax = minimax.Minimax(test_minimax.game, test_minimax.game.p1_turn)
+                new_minimax = minimax.Minimax(test_minimax.game)
 
         def test_outputs_a_move(self, run_before_tests):
                 test_minimax = run_before_tests
-                test_minimax.game.execute_turn(6, 4, 4, 4)
+                # test_minimax.game.execute_turn(6, 4, 4, 4)
                 assert isinstance(test_minimax.execute_turn(), list)
 
 class TestValueOfMoves:
@@ -37,18 +37,19 @@ class TestValueOfMoves:
 class TestAllAvailableMoves:
         def test_returns_available_moves_white(self, run_before_tests):
                 test_minimax = run_before_tests
+                print(test_minimax.available_moves())
                 assert test_minimax.available_moves() == [
-                        [[2, 0],[2, 2]],
-                        [[0, 2],[0, 6]],
-                        [[2, 5], [2, 7]],
-                        [[2, 0], [3, 0]],
-                        [[2, 1], [3, 1]],
-                        [[2, 2], [3, 2]],
-                        [[2, 3], [3, 3]],
-                        [[2, 4], [3, 4]],
-                        [[2, 5], [3, 5]],
-                        [[2, 6], [3, 6]],
-                        [[2, 7], [3, 7]]
+                        [[4, 0], [5, 0]], 
+                        [[4, 1], [5, 1]], 
+                        [[4, 2], [5, 2]], 
+                        [[4, 3], [5, 3]], 
+                        [[4, 4], [5, 4]], 
+                        [[4, 5], [5, 5]], 
+                        [[4, 6], [5, 6]], 
+                        [[4, 7], [5, 7]], 
+                        [[5, 0], [5, 2]], 
+                        [[7, 2], [7, 6]], 
+                        [[5, 5], [5, 7]]
                         ]
 
         def test_returns_available_moves_black(self, run_before_tests):
