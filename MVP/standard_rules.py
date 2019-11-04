a45d2f886fe18b53cc3f4fe1547e97af6722c344
Mike <mjwalshe@gmail.com>
2019-11-04 16:27:11 +0000

    Remove old constraints from standard rules

--- MVP/standard_rules.py
+++ MVP/standard_rules.py
@@ -23,7 +23,8 @@ class StandardRules:
         
         def __init__(self):
                 self.starting_board = [
-                        [rook.Rook("Black"),knight.Knight("Black"),bishop.Bishop("Black"),queen.Queen("Black"),king.King("Black"),bishop.Bishop("Black"),knight.Knight("Black"),rook.Rook("Black")],
+                        [rook.Rook("Black"),knight.Knight("Black"),bishop.Bishop("Black"),queen.Queen("Black"),
+                         king.King("Black"),bishop.Bishop("Black"),knight.Knight("Black"),rook.Rook("Black")],
                         [pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black"),
                         pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black"),pawn.Pawn("Black")],
                         ["-","-","-","-","-","-","-","-"],
@@ -32,17 +33,6 @@ class StandardRules:
                         ["-","-","-","-","-","-","-","-"],
                         [pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White"),
                         pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White"),pawn.Pawn("White")],
-                        [rook.Rook("White"),knight.Knight("White"),bishop.Bishop("White"),queen.Queen("White"),king.King("White"),bishop.Bishop("White"),knight.Knight("White"),rook.Rook("White")]
+                        [rook.Rook("White"),knight.Knight("White"),bishop.Bishop("White"),queen.Queen("White"),
+                         king.King("White"),bishop.Bishop("White"),knight.Knight("White"),rook.Rook("White")]
                         ]
-                
-        def invalid_move(self, board, start_row, start_col, end_row, end_col):
-                current_board = board
-                piece_to_move = current_board[start_row][start_col]
-                return any(
-                        [self.check_within_board_boundary(end_row,end_col),
-                        (piece_to_move.illegal_directions(current_board, start_row, start_col, end_row, end_col)) # checks pawn allowed vectors
-                        ]
-                        )
-
-        def check_within_board_boundary(self, end_row, end_col):
-                return (end_row > 7 or end_col > 7 or end_row < 0 or end_col < 0)
