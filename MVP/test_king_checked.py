744464e02befc7e7973d32862996b72e49b50d01
Mike <mjwalshe@gmail.com>
2019-11-04 16:17:40 +0000

    Fix king checked tests

--- MVP/test_king_checked.py
+++ MVP/test_king_checked.py
@@ -12,7 +12,7 @@ class TestKingInCheck:
 
         def test_starting_king_not_in_check(self, run_before_tests):
                 test_game = run_before_tests
-                assert test_game.board.board[0][4].in_check(test_game.board.board, 0, 4) == False
+                assert test_game.board[0][4].in_check(test_game.board, 0, 4) == False
 
         def test_king_not_in_check_from_own_pieces(self, run_before_tests):
                 test_game = run_before_tests
@@ -21,14 +21,14 @@ class TestKingInCheck:
                 test_game.execute_turn(7,4,6,4)
                 test_game.execute_turn(0,4,1,4)
                 test_game.execute_turn(6,4,5,4)
-                assert test_game.board.board[5][4].in_check(test_game.board.board, 5, 4) == False
+                assert test_game.board[5][4].in_check(test_game.board, 5, 4) == False
                 test_game.execute_turn(1,4,2,4)
-                assert test_game.board.board[2][4].in_check(test_game.board.board, 2, 4) == False
+                assert test_game.board[2][4].in_check(test_game.board, 2, 4) == False
                 test_game.execute_turn(5,4,5,5)
-                assert test_game.board.board[5][5].in_check(test_game.board.board, 5, 5) == False
+                assert test_game.board[5][5].in_check(test_game.board, 5, 5) == False
                 test_game.execute_turn(0,3,2,5) # black queen puts king in check
-                assert test_game.board.board[5][5].in_check(test_game.board.board, 5, 5) == True # white king in check?
-                assert test_game.board.board[2][4].in_check(test_game.board.board, 2, 4) == False # black king in check?
+                assert test_game.board[5][5].in_check(test_game.board, 5, 5) == True # white king in check?
+                assert test_game.board[2][4].in_check(test_game.board, 2, 4) == False # black king in check?
 
         def test_king_can_be_in_check(self, run_before_tests):
                 test_game = run_before_tests
@@ -36,7 +36,7 @@ class TestKingInCheck:
                 test_game.execute_turn(1,2,2,2)
                 test_game.execute_turn(6,7,4,7)
                 test_game.execute_turn(0,3,3,0)
-                assert test_game.board.board[7][4].in_check(test_game.board.board, 7, 4) == True
+                assert test_game.board[7][4].in_check(test_game.board, 7, 4) == True
 
         def test_king_in_check_from_bishop(self, run_before_tests):
                 test_game = run_before_tests
@@ -44,7 +44,7 @@ class TestKingInCheck:
                 test_game.execute_turn(1,4,3,4)
                 test_game.execute_turn(6,7,4,7)
                 test_game.execute_turn(0,5,4,1)
-                assert test_game.board.board[7][4].in_check(test_game.board.board, 7, 4) == True
+                assert test_game.board[7][4].in_check(test_game.board, 7, 4) == True
 
         def test_king_in_check_from_knight(self, run_before_tests):
                 test_game = run_before_tests
@@ -55,5 +55,5 @@ class TestKingInCheck:
                 test_game.execute_turn(4,7,3,5)
                 test_game.execute_turn(4,0,5,0)
                 test_game.execute_turn(3,5,1,6)
-                assert test_game.board.board[0][4].in_check(test_game.board.board, 0, 4) == True
+                assert test_game.board[0][4].in_check(test_game.board, 0, 4) == True
 
