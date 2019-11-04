c0279c645cadbc517a24f20723a7acdd5e92b856
Mike <mjwalshe@gmail.com>
2019-11-04 16:29:17 +0000

    Update test for check method name

--- MVP/turn.py
+++ MVP/turn.py
@@ -23,7 +23,7 @@ class Turn:
             raise ValueError("Invalid Move")
         # if self.__king_in_check(start_row, start_col, end_row, end_col):
         #     raise ValueError("Invalid Move")
-        if self.update_board(start_row, start_col, end_row, end_col) == 'invalid move':
+        if self.check_if_move_into_check(start_row, start_col, end_row, end_col) == 'invalid move':
             raise ValueError("Invalid Move")
         self.board[start_row][start_col] = "-"
         self.__store_piece_if_struck(end_row, end_col)
@@ -31,14 +31,7 @@ class Turn:
         piece_to_move.increment_counter()
         self.__check_pawn_promotion(piece_to_move, end_row, end_col)
 
-    # private methods
-    # def __king_in_check(self, start_row, start_col, end_row, end_col):
-    #     if self.update_board(start_row, start_col, end_row, end_col) == 'invalid move':
-    #         return True
-    #     else:
-    #         return False
-
-    def update_board(self, start_row, start_col, end_row, end_col):
+    def check_if_move_into_check(self, start_row, start_col, end_row, end_col):
         piece_to_move = self.board[start_row][start_col]
         colour = piece_to_move.colour
         changed_board = copy.deepcopy(self.board)
