853a21d2e307a8c207585b91d0ac2b0204bdde3d
Mike <mjwalshe@gmail.com>
2019-11-05 15:10:38 +0000

    Pass tests for non-random behaviour

--- MVP/test_random_pieces.py
+++ MVP/test_random_pieces.py
@@ -1,11 +1,9 @@
-# it creates a board with 64 elements
-# it creates a rank with a king
-# it creates a rank without a king
-
+import pytest
+import random
+import king
 from piece import Piece
 from king import King
 import standard_rules 
-import pytest
 import random_pieces 
 
 @pytest.fixture(autouse=True)
@@ -22,7 +20,8 @@ class TestRandomPieces:
         def test_ruleset_creates_standard_chess_board(self):
                 test_ruleset = random_pieces.RandomPieces()
                 assert len(test_ruleset.starting_board) == 8
-
-
-
-
+                assert len(test_ruleset.starting_board[0]) == 8
+                
+        # def test_ruleset_creates_a_rank_with_king(self):
+        #         test_ruleset = random_pieces.RandomPieces()
+        #         assert isinstance(test_ruleset.starting_board[0][3], king.King)
