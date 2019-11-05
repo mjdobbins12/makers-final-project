8b4e4e67b79cac8f05dd8aa8134bf24949e4be5b
Dan Gyi <daniel.gyi@gmail.com>
2019-11-05 11:57:53 +0000

    update piece values

--- MVP/knight.py
+++ MVP/knight.py
@@ -4,13 +4,12 @@ class Knight(Piece):
         def __init__(self, colour):
                 self.colour = colour
                 self.name = "Knight"
-                self.value = 3
                 if self.colour == "Black":
                         self.symbol = '♞'
-                        self.value = -3
+                        self.value = -30
                 elif self.colour == "White":
                         self.symbol = "♘"
-                        self.value = 3
+                        self.value = 30
         
 
         def illegal_directions(self, board, start_row, start_col, end_row, end_col):
--- MVP/pawn.py
+++ MVP/pawn.py
@@ -7,10 +7,10 @@ class Pawn(Piece):
 
                 if self.colour == "Black":
                         self.symbol = '♟'
-                        self.value = -1
+                        self.value = -10
                 elif self.colour == "White":
                         self.symbol = "♙"
-                        self.value = 1
+                        self.value = 10
 
         def illegal_directions(self, board, start_row, start_col, end_row, end_col):
                 return any([
