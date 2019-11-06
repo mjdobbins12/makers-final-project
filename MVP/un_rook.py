b3d606c77162514aa6a9ec552730399f612cdf63
Mike <mjwalshe@gmail.com>
2019-11-06 10:54:38 +0000

    Add un_rook - tests passing

---
+++ MVP/un_rook.py
@@ -0,0 +1,18 @@
+from piece import Piece
+from rook import Rook
+
+class UnRook(Rook):
+
+        def __init__(self, colour):
+                self.colour = colour
+                self.name = "Rook"
+                if self.colour == "Black":
+                        self.symbol = '♜'
+                        self.value = -50
+                elif self.colour == "White":
+                        self.symbol = '♖'
+                        self.value = 50
+
+        def invalid_move_types(self, start_row, start_col, end_row, end_col):
+                        return True
+
