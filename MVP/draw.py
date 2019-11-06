466f202237de67663b9ebdf0aa9458aea023a3e1
Mark Dobbins <mjdobbins12@gmail.com>
2019-11-06 17:33:50 +0000

    fix turn parameters in checkmate

--- MVP/draw.py
+++ MVP/draw.py
@@ -21,5 +21,5 @@ class Draw:
             for j in range(0,8):
                 if isinstance(self.board[i][j], Piece) and self.board[i][j].colour == self.current_player:
                     for sq in self.board[i][j].available_moves(self.board, i, j):
-                        stale.append(Turn(self.game.ruleset, self.board, self.game.player_1, self.game.player_2).check_if_self_in_check(i, j, sq[0], sq[1]) == 'invalid move')
+                        stale.append(Turn(self.game.ruleset, self.board, self.game.log, self.game.player_1, self.game.player_2).check_if_self_in_check(i, j, sq[0], sq[1]) == 'invalid move')
         return all(stale)
