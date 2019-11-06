98bac70ec7d41edce4301b4ae6791908a2643bbb
Volker Bergner <volker@bergner.one>
2019-11-06 12:17:33 +0000

    Prototype for multiple channels and games

--- MVP/slack_ui.py
+++ MVP/slack_ui.py
@@ -6,7 +6,9 @@ class Slack:
 
     def __init__(self):
         self.client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
-        self.slack_control = SlackControl()
+        # self.slack_control = SlackControl()
+        self.slack_control = {}
+
 
     def post(self, client, text, channel = '#chess'):
         output = client.chat_postMessage(
@@ -15,19 +17,27 @@ class Slack:
             as_user = True)
 
     def start_listen(self):
-        self.post(self.client, self.__intro_chessy())
+        @slack.RTMClient.run_on(event='channel_joined')
+        def join_channel(**payload):
+            data = payload['data']
+            print(data)
+            self.post(self.client, self.__intro_chessy(), data['channel']['id'])
+            self.slack_control[data['channel']['id']] = SlackControl(data['channel']['id'])
         @slack.RTMClient.run_on(event='message')
         def run_game(**payload):
             data = payload['data']
+            print(data)
+            print (data['channel'])
             web_client = payload['web_client']
             rtm_client = payload['rtm_client']
-            self.slack_control.check_for_start(web_client, data)
-            self.slack_control.check_for_moves(web_client, data)
-            self.slack_control.check_for_stop(web_client, data)
-            self.slack_control.check_for_mode(web_client, data)
-            self.slack_control.check_for_mode_set(web_client, data)
-            self.slack_control.check_for_join(web_client, data)
-            self.slack_control.check_for_AI(web_client, data)
+            self.slack_control[data['channel']].check_for_start(web_client, data)
+            self.slack_control[data['channel']].check_for_start(web_client, data)
+            self.slack_control[data['channel']].check_for_moves(web_client, data)
+            self.slack_control[data['channel']].check_for_stop(web_client, data)
+            self.slack_control[data['channel']].check_for_mode(web_client, data)
+            self.slack_control[data['channel']].check_for_mode_set(web_client, data)
+            self.slack_control[data['channel']].check_for_join(web_client, data)
+            self.slack_control[data['channel']].check_for_AI(web_client, data)
 
         slack_token = os.environ["SLACK_API_TOKEN"]
         rtm_client = slack.RTMClient(token=slack_token)
